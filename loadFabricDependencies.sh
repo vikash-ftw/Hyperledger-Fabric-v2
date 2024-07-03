27961886815b3983647286e0b40be03445539ad8
vikash-ftw <vikashbatham97@gmail.com>
2024-07-03 16:56:28 +0530

    made config changes for fabric v2.5

--- loadFabricDependencies.sh
+++ loadFabricDependencies.sh
@@ -1,10 +1,13 @@
 # Sample Fabric network
 
-# load fabric images 2.1.0 fabric-ca 1.4.7 fabric-couchdb 0.4.20
+# load fabric images 2.1.0 fabric-ca 1.4.7
 #curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.1.0 1.4.7 0.4.20
 
-# load fabric images 2.2.15 fabric-ca 1.5.11 fabric-couchdb latest
-curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.2.15 1.5.11
+# load fabric images 2.2.15 fabric-ca 1.5.11 
+#curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.2.15 1.5.11
+
+# load fabric images 2.5.9 fabric-ca 1.5.11 
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.5.9 1.5.11
 
 echo "========= download completed =========== "
 cp -R fabric-samples/bin .
