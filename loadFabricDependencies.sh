d6ac47b3fbaea8cc4cc286db493f98025269c606
vikash-ftw <vikashbatham97@gmail.com>
2024-09-28 21:33:56 +0530

    removed couchdb version comment from script

--- loadFabricDependencies.sh
+++ loadFabricDependencies.sh
@@ -1,6 +1,6 @@
 # Sample Fabric network
 
-# load fabric images 2.1.0 fabric-ca 1.4.7 fabric-couchdb 0.4.20
+# load fabric images 2.1.0 fabric-ca 1.4.7
 #curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.1.0 1.4.7 0.4.20
 
 # load fabric images 2.2.15 fabric-ca 1.5.11
